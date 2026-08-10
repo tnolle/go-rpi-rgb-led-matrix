ee1cc1ad7c25f738aed59f420bc047c63ba6ca85
Timo Nolle <t.nolle@autodarts.io>
2026-08-10 21:08:59 +0200

    Add barebones Ratatui client

---
+++ rust/src/main.rs
@@ -0,0 +1,13 @@
+use ratatui::widgets::{Block, Paragraph};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ratatui::run(|terminal| {
+        terminal.draw(|frame| {
+            let block = Block::bordered().title("Welcome");
+            let greeting = Paragraph::new("Hello, Ratatui! 🐭").centered().block(block);
+            frame.render_widget(greeting, frame.area());
+        })?;
+        std::thread::sleep(std::time::Duration::from_secs(5));
+        Ok(())
+    })
+}
